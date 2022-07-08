a89ba8ca9df1bdb372aae59f17cd00350ccfd005
dalcomi20 <khjune@gmail.com>
2022-07-08 23:23:33 +0900

    [INIT] NETCODE setup
    - Netcode for gameObjects with Unity Transport setup
    - Server side implementation for moving player prefab
    - Client side request server to move player

---
+++ Assets/Scripts/Common/Managers/XVerseNetworkManager.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+
+/// <summary>
+/// Netcode for Gameobjects
+/// https://docs-multiplayer.unity3d.com/netcode/current/tutorials/helloworld/helloworldintro
+/// </summary>
+public class XVerseNetworkManager : MonoBehaviour
+{
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            StartButtons();
+        }
+        else
+        {
+            StatusLabels();
+
+            SubmitNewPosition();
+        }
+
+        GUILayout.EndArea();
+    }
+
+    static void SubmitNewPosition()
+    {
+        if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
+        {
+            if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient)
+            {
+                foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
+                    NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<NETPlayerMove>().Move();
+            }
+            else
+            {
+                var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+                var player = playerObject.GetComponent<NETPlayerMove>();
+                player.Move();
+            }
+        }
+    }
+
+    static void StartButtons()
+    {
+        if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+        if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+    }
+
+    static void StatusLabels()
+    {
+        var mode = NetworkManager.Singleton.IsHost ?
+            "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+
+        GUILayout.Label("Transport: " +
+            NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+        GUILayout.Label("Mode: " + mode);
+    }
+}
---
+++ Assets/Scripts/Player/NETPlayerMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NETPlayerMove : NetworkBehaviour
+{
+    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+
+    // This is the 'OnPhotonInstantiate' counterpart.
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            Move();
+        }
+    }
+
+    public void Move()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            var randomPosition = GetRandomPositionOnPlane();
+            transform.position = randomPosition;
+            Position.Value = randomPosition;
+        }
+        else
+        {
+            SubmitPositionRequestServerRpc();
+        }
+    }
+
+    [ServerRpc]
+    void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
+    {
+        Position.Value = GetRandomPositionOnPlane();
+    }
+
+    static Vector3 GetRandomPositionOnPlane()
+    {
+        return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+    }
+
+    void Update()
+    {
+        transform.position = Position.Value;
+    }
+}
