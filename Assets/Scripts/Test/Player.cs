372e497098dd8f59312c5818ff09b244d5911a3f
Taegu Han <hantaegu96@naver.com>
2022-07-09 18:53:23 +0900

    [feat] Add ParrelSync, simple multiplayer code

---
+++ Assets/Scripts/Test/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement(){
+        if(isLocalPlayer){
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal*0.1f, moveVertical*0.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    void FixedUpdate(){
+        HandleMovement();
+    }
+}
